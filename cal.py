5ff9a0884aa40c37ab6fa4ecdad8d3ed301c24cc
jw-1113 <densang1113@gmail.com>
2025-09-05 16:45:56 +0900

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def add(num1, num2):
+    return num1 + num2
+
+def dif(num1, num2):
+    return num1 - num2
+
+def mul(num1, num2):
+    return num1 * num2
+
+def div(num1, num2):
+    return num1 / num2
+
+
+# In[2]:
+
+
+
+
